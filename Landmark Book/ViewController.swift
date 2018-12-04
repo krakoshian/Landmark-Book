2f5b81f935b5a06c98f86f30a6cd351c3a1e187d
Kadir Uludağ <kadiruludag@kadir-macbook-pro.local>
2018-12-04 21:10:53 +0300

    Initial Commit

---
+++ Landmark Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Book
+//
+//  Created by Kadir Uludağ on 4.12.2018.
+//  Copyright © 2018 Kadir Uludağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
